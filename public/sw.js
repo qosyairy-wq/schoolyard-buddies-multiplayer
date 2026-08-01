aea0a52fcfabb9f3c1f32e2896d390ec24c45776
qosyairy-wq <qosyairy@gmail.com>
2026-08-01 16:43:29 +0800

    Stage 14.2.3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='schoolyard-buddies-pwa-v14-2-2';
+const CACHE='schoolyard-buddies-pwa-v14-2-3';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
