8fcff52235c7abd00c0fd576a35358c19c82ed5c
qosyairy-wq <qosyairy@gmail.com>
2026-08-01 23:18:19 +0800

    Add full-screen mobile quiz and paused menu hub

---
+++ public/README_MOBILE_V14_3_3.txt
@@ -0,0 +1,6 @@
+Schoolyard Buddies Mobile PWA v14.3.3
+
+- Full-screen quiz, assignment and story focus mode
+- Full-screen paused Menu Hub
+- Gameplay HUD hidden inside menu
+- World renderer paused while full-screen UI is open
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='schoolyard-buddies-pwa-v14-3-2';
+const CACHE='schoolyard-buddies-pwa-v14-3-3';
 const CORE=['./','./index.html','./game.html','./manifest.webmanifest','./assets/schoolyard-logo.png','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
